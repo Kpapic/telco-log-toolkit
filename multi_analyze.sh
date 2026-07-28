eb7e5f6e9fea43ddc2457e95d06973c291261a9e
Krešimir Papić <kresimir.papic@hotmail.com>
2026-07-28 12:40:51 +0200

    Add total match count to report

--- multi_analyze.sh
+++ multi_analyze.sh
@@ -15,11 +15,15 @@ read -p "Please enter search term: " search_term
 echo "Searching for $search_term ..."
 
 echo
-
+total=0
 show_header
 
 for file in *.log
 do 
 count=$(grep "$search_term" "$file" | wc -l)
+total=$((total + count))
 echo "$file" "$count"
 done
+
+echo
+echo "Total matches: $total"
